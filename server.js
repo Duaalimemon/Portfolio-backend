856f6e69d6abd5b626f154460f220b960237dd41
Dua Ali Memon <K23SW023@students.muetkhp.edu.pk>
2026-01-18 14:31:29 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 
 // MongoDB URI Check
 const MONGO_URI = process.env.MONGO_URI;
@@ -49,3 +50,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
 });
+
